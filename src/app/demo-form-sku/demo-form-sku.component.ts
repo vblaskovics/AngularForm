836b76c0ef26124fb9b8144d16f1cea33ad74274
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-04-14 10:02:54 +0200

    Add simple template form

---
+++ src/app/demo-form-sku/demo-form-sku.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form-sku',
+  templateUrl: './demo-form-sku.component.html',
+  styleUrls: ['./demo-form-sku.component.css']
+})
+export class DemoFormSkuComponent implements OnInit {
+
+  constructor() { }
+
+  onSubmit(form: any) :void {
+    console.log('submitted values:', form);
+  }
+
+  ngOnInit(): void {
+  }
+
+}

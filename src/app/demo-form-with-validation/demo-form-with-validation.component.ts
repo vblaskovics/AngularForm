0bce9dd886ae0c869c2d56becfe497bcf633261b
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-04-14 13:41:05 +0200

    Add custom validator

--- src/app/demo-form-with-validation/demo-form-with-validation.component.ts
+++ src/app/demo-form-with-validation/demo-form-with-validation.component.ts
@@ -1,32 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+function skuValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^123/)) {
+    return {
+      invalidSku: true,
+    };
+  }
+}
 
 @Component({
   selector: 'app-demo-form-with-validation',
   templateUrl: './demo-form-with-validation.component.html',
-  styleUrls: ['./demo-form-with-validation.component.css']
+  styleUrls: ['./demo-form-with-validation.component.css'],
 })
 export class DemoFormWithValidationComponent implements OnInit {
   myForm: FormGroup;
-  sku: AbstractControl;
-  sku2: AbstractControl;
+  // sku: AbstractControl;
+  // sku2: AbstractControl;
 
-  constructor(fb: FormBuilder) { 
+  constructor(fb: FormBuilder) {
     this.myForm = fb.group({
-      'sku': ['', Validators.required ],
-      'sku2': ['', Validators.required ]
+      sku: ['', Validators.compose([Validators.required, skuValidator])],
+      sku2: ['', Validators.required],
+      sku3: [''],
     });
 
-    this.sku = this.myForm.controls['sku'];
-    this.sku2 = this.myForm.controls['sku2'];
-    
+    // this.sku = this.myForm.controls['sku'];
+    // this.sku2 = this.myForm.controls['sku2'];
   }
 
-  onSubmit(formValue: any) :void {
+  onSubmit(formValue: any): void {
     console.log('submitted values:', formValue);
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

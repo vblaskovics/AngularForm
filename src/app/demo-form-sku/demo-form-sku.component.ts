a78089ac7476019e2b749f29ad8b21004bd5a9c9
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-04-14 10:47:45 +0200

    Add form controls

--- src/app/demo-form-sku/demo-form-sku.component.ts
+++ src/app/demo-form-sku/demo-form-sku.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-demo-form-sku',
@@ -10,7 +11,7 @@ export class DemoFormSkuComponent implements OnInit {
   constructor() { }
 
   onSubmit(form: any) :void {
-    console.log('submitted values:', form);
+    console.log('submitted values:', form.value);
   }
 
   ngOnInit(): void {

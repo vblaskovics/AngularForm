095e7e6392d673c582292477b675cfd346b988a8
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-04-14 11:10:07 +0200

    Add reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoFormSkuComponent
+    DemoFormSkuComponent,
+    DemoFormSkuWithBuilderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/demo-form-sku-with-builder/demo-form-sku-with-builder.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-demo-form-sku-with-builder',
+  templateUrl: './demo-form-sku-with-builder.component.html',
+  styleUrls: ['./demo-form-sku-with-builder.component.css']
+})
+export class DemoFormSkuWithBuilderComponent implements OnInit {
+  myForm: FormGroup;
+
+  constructor(fb: FormBuilder) { 
+    this.myForm = fb.group({
+      'sku': ['ABC123']
+    });
+  }
+
+  onSubmit(formValue: any) :void {
+    console.log('submitted values:', formValue);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
